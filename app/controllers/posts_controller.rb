b2145acd2959a69e5be39903c575928e8ea6d53f
yuya uchida <u.yuya0616ll@gmail.com>
2021-02-07 01:18:27 +0900

    create new action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to new_post_path
+  end
+
+  def show
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
