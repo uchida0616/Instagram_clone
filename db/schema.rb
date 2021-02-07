5b18e38576d96de82423350486e724bb9d0dd03d
yuya uchida <u.yuya0616ll@gmail.com>
2021-02-07 12:35:44 +0900

    set unique constraints on email addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_031353) do
+ActiveRecord::Schema.define(version: 2021_02_07_032933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_07_031353) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
