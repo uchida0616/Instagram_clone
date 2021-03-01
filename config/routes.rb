152e91e632307fec4e9b991afa928d699a52a955
yuya uchida <u.yuya0616ll@gmail.com>
2021-03-01 18:34:26 +0900

    modify root.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  root to: 'users#new'
   resources :favorites, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users do
