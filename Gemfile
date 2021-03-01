327bd027579ce9ced6b1fb9c4ec4b54351c26e02
yuya uchida <u.yuya0616ll@gmail.com>
2021-03-01 15:46:51 +0900

    init

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '3.1.11'
 gem 'carrierwave', '~> 2.0'
 gem 'mini_magick'
+gem 'faker'
 gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   gem 'dotenv-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :post
-  
-  validates :name, presence: true, length: { maximum: 30 }
+
+  validates :name, presence: true
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
